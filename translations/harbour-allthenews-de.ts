088a633a3883a3abf912388d2aa53827e2c38b3f
Mark Washeim <blueprint@poetaster.de>
2022-05-15 16:16:38 +0200

    Updated german translation.

--- translations/harbour-allthenews-de.ts
+++ translations/harbour-allthenews-de.ts
@@ -5,11 +5,11 @@
     <name>About</name>
     <message>
         <source>Feed by</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Feed von</translation>
     </message>
     <message>
         <source>News API can provide headlines from 70 worldwide source.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">News Api liefert Nachrichten von 70 Anbieter weltweit.</translation>
     </message>
     <message>
         <source>(c) poetaster, </source>
@@ -17,33 +17,33 @@
     </message>
     <message>
         <source>Icon from</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Icon von</translation>
     </message>
 </context>
 <context>
     <name>CoverPage</name>
     <message>
         <source>My Cover</source>
-        <translation>Mein Cover</translation>
+        <translation>Meine Cover</translation>
     </message>
 </context>
 <context>
     <name>SourcesPage</name>
     <message>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Über</translation>
     </message>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Einstellungen</translation>
     </message>
     <message>
         <source>Search news</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Suchen</translation>
     </message>
     <message>
         <source>Nothing to show now</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Nichts zum anzeigen bereit</translation>
     </message>
 </context>
 </TS>
